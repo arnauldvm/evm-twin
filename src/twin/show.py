a003fdbc3e90543ddbd990ca23543092ee010355
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2024-04-10 01:28:54 +0200

    add(show.py): load and display image

--- src/twin/show.py
+++ src/twin/show.py
@@ -1,7 +1,12 @@
 import argparse
 import logging
 
+import numpy
+import cv2 as cv
+
 _logger = logging.getLogger(__name__)
+
+# Adapted from https://docs.opencv.org/4.x/db/deb/tutorial_display_image.html
 
 
 def add_parser(subparsers: argparse.Action) -> None:
@@ -12,4 +17,16 @@ def add_parser(subparsers: argparse.Action) -> None:
 
 def run(args: argparse.Namespace) -> None:
     _logger.debug("Loading image from file %r", args.input_file.name)
-    pass
+    try:
+        # img = cv.imread(args.input_file.name)
+        #   this re-opens the file, and cannot read from stdin
+        image_bytes = numpy.asarray(bytearray(args.input_file.read()), dtype=numpy.uint8)
+        img = cv.imdecode(image_bytes, cv.IMREAD_UNCHANGED)
+    finally:
+        args.input_file.close()
+    if img is None:
+        raise Exception("Could not read the image.")
+
+    _logger.debug("Displaying image")
+    cv.imshow("Display window", img)
+    _ = cv.waitKey(0)
